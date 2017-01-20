9666a2b9b57d134b80e5dde1951be98daeaa2eca
Hiromu Yokokura <yokokura@is.s.u-tokyo.ac.jp>
2017-01-20 19:52:32 +0900

    detect intersections

--- lis2/unity-sample-environment/Assets/Scripts/Environment.cs
+++ lis2/unity-sample-environment/Assets/Scripts/Environment.cs
@@ -26,41 +26,57 @@ namespace MLPlayer {
 				Destroy (i.gameObject);
 			}
 			for (int i=0; i<itemCount; i++) {	
-				int itemId = UnityEngine.Random.Range(0, itemPrefabs.Count);
+				while (true) {
+					int itemId = UnityEngine.Random.Range (0, itemPrefabs.Count);
 
-				Vector3 pos = new Vector3 (
-					UnityEngine.Random.Range (-areaSize, areaSize),
-					itemPrefabs[itemId].transform.position.y,
-					UnityEngine.Random.Range (-areaSize, areaSize));
-				Quaternion q = Quaternion.Euler (
-					270,
-					UnityEngine.Random.Range (0f, 360f),
-					0
-				);
+					Vector3 pos = new Vector3 (
+						             UnityEngine.Random.Range (-areaSize, areaSize),
+						             itemPrefabs [itemId].transform.position.y,
+						             UnityEngine.Random.Range (-areaSize, areaSize));
+					Quaternion q = Quaternion.Euler (
+						              270,
+						              UnityEngine.Random.Range (0f, 360f),
+						              0
+					              );
 
-				pos += transform.position;
+					pos += transform.position;
 
-				GameObject obj = (GameObject)GameObject.Instantiate 
-					(itemPrefabs[itemId], pos, q);
-				obj.transform.parent = transform;
+					GameObject obj = (GameObject)GameObject.Instantiate 
+					(itemPrefabs [itemId], pos, q);
+
+					if (!detectIntersecton (obj)) {
+						obj.transform.parent = transform;
+						break;
+					} else {
+						Destroy (obj);
+					}
+				}
 			}
 
 			for (int i=0; i<chairCount; i++) {	
-				int itemId = UnityEngine.Random.Range(0, chairPrefabs.Count);
-				Vector3 pos = new Vector3 (
-					UnityEngine.Random.Range (-areaSize, areaSize),
-					chairPrefabs[itemId].transform.position.y,
-					UnityEngine.Random.Range (-areaSize, areaSize));
-				Quaternion q = Quaternion.Euler (
-					chairPrefabs[itemId].transform.rotation.x,
-					UnityEngine.Random.Range (0f, 360f),
-					0
-					);
+				while (true) {
+					int itemId = UnityEngine.Random.Range (0, chairPrefabs.Count);
+					Vector3 pos = new Vector3 (
+						             UnityEngine.Random.Range (-areaSize, areaSize),
+						             chairPrefabs [itemId].transform.position.y,
+						             UnityEngine.Random.Range (-areaSize, areaSize));
+					Quaternion q = Quaternion.Euler (
+						              chairPrefabs [itemId].transform.rotation.x,
+						              UnityEngine.Random.Range (0f, 360f),
+						              0
+					              );
 
-				pos += transform.position;
-				GameObject obj = (GameObject)GameObject.Instantiate 
-					(chairPrefabs[itemId], pos, q);
-				obj.transform.parent = transform;
+					pos += transform.position;
+					GameObject obj = (GameObject)GameObject.Instantiate 
+					(chairPrefabs [itemId], pos, q);
+
+					if (!detectIntersecton (obj)) {
+						obj.transform.parent = transform;
+						break;
+					} else {
+						Destroy (obj);
+					}
+				}
 			}
 		}
 		// return a all active game objects
@@ -86,6 +102,17 @@ namespace MLPlayer {
 			} else {
 				return false;
 			}
+		}
+
+		public bool detectIntersecton(GameObject obj) {
+			Bounds a = obj.GetComponent<Renderer> ().bounds;
+			foreach (Transform i in transform) {
+				Bounds b = i.gameObject.GetComponent<Renderer> ().bounds;
+				if (a.Intersects (b)) {
+					return true;
+				}
+			}
+			return false;
 		}
 	}
 }
